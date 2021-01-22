e2c63b6b8773d4c6b6d02b57641ce8bc5118a15c
Austin Smith <asmit2222@gmail.com>
2021-01-22 18:17:37 -0500

    created animation css file

--- client/index.js
+++ client/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./css/index.css";
 import "./css/syllabusPages.css";
 import "./css/desktop.css";
+import "./css/animations.css";
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
