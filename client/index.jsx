9645b51bf0f362698aa092359c78e46fcd75d33c
Austin Smith <asmit2222@gmail.com>
2021-01-27 17:28:32 -0500

    lazy loading stylesheets

--- client/index.jsx
+++ client/index.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./css/index.css";
-// import "./css/syllabusPages.css";
 import "./css/desktop.css";
 import "./css/animations.css";
 import App from "./pages/App.jsx";
--- webpack.config.js
+++ webpack.config.js
@@ -45,7 +45,7 @@ module.exports = {
   },
   plugins: [
     new CompressionPlugin(),
-    new BundleAnalyzerPlugin()
+    // new BundleAnalyzerPlugin()
   ]
 
 };
