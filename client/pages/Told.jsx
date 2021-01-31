72175e9b46c0dac0df8ac9c71db27f7971149e3f
Austin Smith <asmit2222@gmail.com>
2021-01-30 23:26:52 -0500

    added to told page

--- client/pages/Told.jsx
+++ client/pages/Told.jsx
@@ -1,11 +1,23 @@
 import React, { Component } from "react";
-import axios from 'axios';
+import axios from "axios";
+import Dropdown from "react-dropdown";
+import "react-dropdown/style.css";
+
+const options = ["05", "14", "23", "32"];
+
+const defaultOption = options[0];
+
+const options2 = ["??", "??", "??", "??"];
+
+const defaultOption2 = options2[0];
 
 class Told extends Component {
   constructor(props) {
     super(props);
     this.state = {
       headwind: null,
+      KNSEMetar: "",
+      KNGPMetar: "",
     };
     this.temperatureKNSE;
     this.windSpeedKNSE;
@@ -26,12 +38,14 @@ class Told extends Component {
       runwayHeading = 320;
     }
     // Math.cos uses radians. Conversion is Radians = Angle in degrees x PI / 180.
-    let windRadian = windDirection * Math.PI / 180;
-    let runwayRadian = runwayHeading * Math.PI / 180;
+    let windRadian = (windDirection * Math.PI) / 180;
+    let runwayRadian = (runwayHeading * Math.PI) / 180;
     this.setState({
-      headwind: Math.trunc(Math.cos(Math.abs(windRadian - runwayRadian)) * windSpeed),
+      headwind: Math.trunc(
+        Math.cos(Math.abs(windRadian - runwayRadian)) * windSpeed
+      ),
     });
-  }
+  };
 
   componentDidMount() {
     axios
@@ -44,6 +58,10 @@ class Told extends Component {
         this.windSpeedKNSE = res.data.wind_speed.value;
         this.windDirectionKNSE = res.data.wind_direction.value;
         this.setHeadwindKNSE(this.windDirectionKNSE, this.windSpeedKNSE);
+
+        this.setState({
+          KNSEMetar: res.data.sanitized,
+        });
       });
 
     axios
@@ -52,6 +70,10 @@ class Told extends Component {
       })
       .then((res) => {
         console.log("here: ", res);
+
+        this.setState({
+          KNGPMetar: res.data.sanitized,
+        });
       });
   }
 
@@ -61,9 +83,66 @@ class Told extends Component {
         <section className={`section container1`}>
           <div className="container">
             <h1 className="title">TOLD</h1>
-          </div>
-          <div>
-            Headwind: {this.state.headwind}
+            <h2 className="metarTitle">NAS WHITING FIELD</h2>
+
+            <div className="KNSEMetar">{this.state.KNSEMetar}</div>
+
+            <div className="toldForRunway">
+              <span className="toldForRunwayText">TOLD FOR RUNWAY</span>
+              <span>
+                <Dropdown
+                  options={options}
+                  onChange={this._onSelect}
+                  value={defaultOption}
+                  placeholder="Select an option"
+                />
+              </span>
+            </div>
+
+            <div className="smallerText">
+              MIN TORQUE AT 60 KIAS:<span className="toldData">{` data`}</span>
+            </div>
+            <div className="smallerText">
+              TAKEOFF DISTANCE (FLAPS T/O):
+              <span className="toldData">{` data`}</span>
+            </div>
+            <div className="smallerText">
+              MAX DRY ABORT SPEED:<span className="toldData">{` data`}</span>
+            </div>
+            <div className="smallerText marginBottomTold">
+              MAX WET ABORT SPEED:<span className="toldData">{` data`}</span>
+            </div>
+
+            <h2 className="metarTitle">NAS CORPUS CHRISTI</h2>
+            <div className="KNGPMetar">{this.state.KNGPMetar}</div>
+
+            <div className="toldForRunway">
+              <span className="toldForRunwayText">TOLD FOR RUNWAY</span>
+              <span>
+                <Dropdown
+                  options={options2}
+                  onChange={this._onSelect2}
+                  value={defaultOption2}
+                  placeholder="Select an option"
+                />
+              </span>
+            </div>
+
+            <div className="smallerText">
+              MIN TORQUE AT 60 KIAS:<span className="toldData">{` data`}</span>
+            </div>
+            <div className="smallerText">
+              TAKEOFF DISTANCE (FLAPS T/O):
+              <span className="toldData">{` data`}</span>
+            </div>
+            <div className="smallerText">
+              MAX DRY ABORT SPEED:<span className="toldData">{` data`}</span>
+            </div>
+            <div className="smallerText marginBottomTold">
+              MAX WET ABORT SPEED:<span className="toldData">{` data`}</span>
+            </div>
+
+            <h2 className="metarTitle">MANUAL ENTRY</h2>
           </div>
         </section>
       </React.Fragment>
