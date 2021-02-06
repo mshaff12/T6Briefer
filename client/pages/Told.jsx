a0895c01c7aa79609fb19cdee9ba7e0f81119047
mshaffer <mshaffer0308@gmail.com>
2021-02-05 18:43:58 -0600

    Added Manual Entry algorithms

--- client/pages/Told.jsx
+++ client/pages/Told.jsx
@@ -19,6 +19,7 @@ class Told extends Component {
     this.state = {
       headwindKNSE: 10,
       headwindKNGP: 15,
+      headwindManual: null,
       runwayHeading: 32,
       stringHeading: "32",
       KNSEMetar: "",
@@ -28,12 +29,16 @@ class Told extends Component {
       maxAbortWetKNSE: null,
       maxAbortDryKNGP: null,
       maxAbortWetKNGP: null,
+      maxAbortDryManual: null,
+      maxAbortWetManual: null,
       minPower60KNSE: null,
       minPower60KNGP: null,
+      minPowerManual: null,
       metarLoadingKNSE: true,
       metarLoadingKNGP: true,
       takeoffDistKNSE: null,
-      takeoffDistKNGP: null
+      takeoffDistKNGP: null,
+      takeoffDistManual: null
     };
     this.temperatureKNSE;
     this.windSpeedKNSE;
@@ -78,23 +83,24 @@ class Told extends Component {
   };
 
   setHeadwindKNGP = (windDirection, windSpeed, runwayHeading) => {
-    /*let runwayHeading;
-    // rwys at KNSE are 05, 14, 23, and 32
-    if (windDirection > 5 && windDirection <= 95) {
-      runwayHeading = 50;
-    } else if (windDirection > 95 && windDirection <= 185) {
-      runwayHeading = 140;
-    } else if (windDirection > 185 && windDirection <= 275) {
-      runwayHeading = 230;
-    } else {
-      runwayHeading = 320;
-    } */
-
     // Math.cos uses radians. Conversion is Radians = Angle in degrees x PI / 180.
     let windRadian = (windDirection * Math.PI) / 180;
     let runwayRadian = (runwayHeading * Math.PI) / 180;
     this.setState({
       headwindKNGP: Math.floor(
+        Math.cos(Math.abs(windRadian - runwayRadian)) * windSpeed
+      ),
+    });
+    this.activateToldModal = this.activateToldModal.bind(this);
+    this.exitToldModal = this.exitToldModal.bind(this);
+  };
+
+  setHeadwindManual = (windDirection, windSpeed, runwayHeading) => {
+    // Math.cos uses radians. Conversion is Radians = Angle in degrees x PI / 180.
+    let windRadian = (windDirection * Math.PI) / 180;
+    let runwayRadian = (runwayHeading * Math.PI) / 180;
+    this.setState({
+      headwindManual: Math.floor(
         Math.cos(Math.abs(windRadian - runwayRadian)) * windSpeed
       ),
     });
@@ -180,6 +186,48 @@ class Told extends Component {
     } else if (temperature <= 50) {
       this.setState({
         minPower60KNGP: 86
+      })
+    } else {
+      throw "Fuck this guy, he shouldn't be flying. Temperature exceeds our bounds."
+    }
+  }
+
+  minPower60Manual = (temperature) => {
+    if (temperature <= 42) {
+      this.setState({
+        minPower60Manual: 100
+      })
+    } else if (temperature <= 43) {
+      this.setState({
+        minPower60Manual: 98
+      })
+    } else if (temperature <= 44) {
+      this.setState({
+        minPower60Manual: 96
+      })
+    } else if (temperature <= 45) {
+      this.setState({
+        minPower60Manual: 94
+      })
+    } else if (temperature <= 46) {
+      this.setState({
+        minPower60Manual: 93
+      })
+    } else if (temperature <= 47) {
+      this.setState({
+        minPower60Manual: 91
+      })
+    } else if (temperature <= 48) {
+      this.setState({
+        minPower60Manual: 89
+      })
+    } else if (temperature <= 49) {
+      this.setState({
+        minPower60Manual: 87
+      })
+    } else if (temperature <= 50) {
+      this.setState({
+        minPower60Manual: 86
       })
     } else {
       throw "Fuck this guy, he shouldn't be flying. Temperature exceeds our bounds."
@@ -368,6 +416,119 @@ class Told extends Component {
 
   }
 
+  maxAbortSpeedManual = (headwind, temperature, runwayLength) => {
+    // Need to add try {} catch{] to program to account for out of limits entry
+    // I tried adding isNaN() for temperature and headwind, but it didn't work
+    if (temperature > 50 || temperature < 0 ||
+        headwind > 40 || headwind < -20) {
+      throw "Input is out of limits.";
+    }
+    let speedCacheDry;
+    let speedCacheWet;
+    if (runwayLength == 5000) {
+      //runwayDistance = 5000
+      speedCacheDry = [
+        [71, 86, 100, 104, 107, 110, 114],
+        [70, 84, 98, 101, 104, 107, 110],
+        [68, 80, 94, 97, 100, 104, 108],
+        [60, 72, 90, 94, 97, 101, 104],
+        [59, 73,	88,	91,	94,	98,	102],
+        [58, 71,	86,	89,	92,	96,	100]
+      ];
+      speedCacheWet = [
+        [49, 60,	72,	76,	77,	78,	84],
+        [48, 59,	70,	75,	76,	77,	78],
+        [46, 56,	68,	70,	72,	76,	77],
+        [40, 54,	62,	68,	70,	75,	76],
+        [38, 54,	60,	67,	68,	70,	74],
+        [36, 52,	60,	62,	67,	68,	72]
+      ];
+    } else if (runwayLength == 6000) {
+      //runway distance = 6000
+        speedCacheDry = [
+          [84, 100, 108, 110, 112, 116, 120],
+          [74, 88, 102, 105, 109, 112, 116],
+          [71, 84, 98, 100, 104, 108, 110],
+          [69, 80, 96, 99, 102, 106, 109],
+          [69, 80, 95, 98, 102, 105, 109],
+          [69, 80, 94, 97, 101, 104, 108]
+        ];
+        speedCacheWet = [
+          [60, 72, 77, 78, 82, 86, 88],
+          [54, 60, 76, 77 ,78, 82, 86],
+          [52, 59, 70, 72, 76, 77, 78],
+          [47, 56, 68, 72, 76, 77, 78],
+          [47, 56, 68, 72, 76, 77, 78],
+          [47, 56, 68, 70, 75, 76, 77]
+        ];
+      } else if (runwayLength == 8000) {
+      //runwayDistance = 8000
+      speedCacheDry = [
+        [101, 115, 130,	133, 136,	139, 142],
+        [94, 110,	122, 125,	128, 131,	134],
+        [90, 102,	118, 120,	123, 127,	130],
+        [88, 101,	116, 119,	122, 125, 128],
+        [84, 98, 112, 115, 118, 122, 125],
+        [80, 95, 110, 113, 116, 120, 123]
+      ];
+      speedCacheWet = [
+        [75, 85, 100, 105, 106, 108, 110],
+        [68, 78, 95, 97, 99, 102, 106],
+        [62, 76, 87, 88, 96, 97, 100],
+        [60, 75, 86, 87, 95, 97, 99],
+        [59, 72, 82, 85, 87, 95, 97],
+        [56, 68, 78, 84, 86, 88, 96]
+      ];
+      
+    } else {
+      throw "Invalid Runway Length";
+    }
+
+    headwind += 20;
+    headwind /= 10;
+    temperature /= 10;
+    let temperatureIdx1 = Math.floor(temperature);
+    let temperatureIdx2 = Math.ceil(temperature);
+    let headwindIdx1 = Math.floor(headwind);
+    let headwindIdx2 = Math.ceil(headwind);
+    let minuend =
+      (speedCacheDry[temperatureIdx1][headwindIdx2] -
+        speedCacheDry[temperatureIdx1][headwindIdx1]) *
+        (headwind - headwindIdx1) +
+      speedCacheDry[temperatureIdx1][headwindIdx1];
+    let subtrahend =
+      (speedCacheDry[temperatureIdx2][headwindIdx2] -
+        speedCacheDry[temperatureIdx2][headwindIdx1]) *
+        (headwind - headwindIdx1) +
+      speedCacheDry[temperatureIdx2][headwindIdx1];
+    let maxAbort =
+      (minuend - subtrahend) * (temperature - temperatureIdx1) + subtrahend;
+    this.setState({
+      maxAbortDryManual: Math.ceil(maxAbort),
+    });
+
+    temperatureIdx1 = Math.floor(temperature);
+    temperatureIdx2 = Math.ceil(temperature);
+    headwindIdx1 = Math.floor(headwind);
+    headwindIdx2 = Math.ceil(headwind);
+    minuend =
+      (speedCacheWet[temperatureIdx1][headwindIdx2] -
+        speedCacheWet[temperatureIdx1][headwindIdx1]) *
+        (headwind - headwindIdx1) +
+      speedCacheWet[temperatureIdx1][headwindIdx1];
+    subtrahend =
+      (speedCacheWet[temperatureIdx2][headwindIdx2] -
+        speedCacheWet[temperatureIdx2][headwindIdx1]) *
+        (headwind - headwindIdx1) +
+      speedCacheWet[temperatureIdx2][headwindIdx1];
+    maxAbort =
+      (minuend - subtrahend) * (temperature - temperatureIdx1) + subtrahend;
+    this.setState({
+      maxAbortWetManual: Math.ceil(maxAbort),
+    });
+
+  }
+
   takeoffDistKNSE = (headwind, temperature) => {
     if (temperature > 50 || temperature < 0 ||
         headwind > 40 || headwind < -20) {
@@ -443,6 +604,45 @@ class Told extends Component {
     (minuend - subtrahend) * (temperature - temperatureIdx1) + subtrahend;
   this.setState({
     takeoffDistKNGP: Math.ceil(takeoffDistance),
+  });
+  }
+
+  takeoffDistManual = (headwind, temperature) => {
+    if (temperature > 50 || temperature < 0 ||
+      headwind > 40 || headwind < -20) {
+        throw "Input out of limits.";
+      }
+  
+  let distanceCache = [
+    [2700, 2200, 1500, 1400, 1300, 1200, 1100],
+    [2800, 2300, 1600, 1500, 1400, 1300, 1200],
+    [2800, 2300, 1700, 1600, 1500, 1400, 1300],
+    [2900, 2400, 1800, 1700, 1600, 1500, 1400],
+    [3500, 2800, 2100, 1900, 1800, 1600, 1400],
+    [4200, 3500, 2600, 2500, 2300, 2100, 2000]
+  ];
+
+  headwind += 20;
+  headwind /= 10;
+  temperature /= 10;
+  let temperatureIdx1 = Math.floor(temperature);
+  let temperatureIdx2 = Math.ceil(temperature);
+  let headwindIdx1 = Math.floor(headwind);
+  let headwindIdx2 = Math.ceil(headwind);
+  let minuend =
+    (distanceCache[temperatureIdx1][headwindIdx2] -
+      distanceCache[temperatureIdx1][headwindIdx1]) *
+      (headwind - headwindIdx1) +
+    distanceCache[temperatureIdx1][headwindIdx1];
+  let subtrahend =
+    (distanceCache[temperatureIdx2][headwindIdx2] -
+      distanceCache[temperatureIdx2][headwindIdx1]) *
+      (headwind - headwindIdx1) +
+    distanceCache[temperatureIdx2][headwindIdx1];
+  let takeoffDistance =
+    (minuend - subtrahend) * (temperature - temperatureIdx1) + subtrahend;
+  this.setState({
+    takeoffDistManual: Math.ceil(takeoffDistance),
   });
   }
 
